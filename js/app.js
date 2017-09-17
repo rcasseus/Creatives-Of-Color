17d630d2cbfd8d6b75f546f8906d0dfae4f4173f
rcasseus <rashan.casseus@gmail.com>
2017-09-17 00:50:38 -0400

    got the dat to flow

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@ function displayModal() { // codekit ignore:line
   document.getElementById('demo').innerHTML = "Clicked"; // this worked!!!!!!
   document.getElementById('modal1').style.display = "block"; // this worked!!!!!!
   console.log("Donate button clicked");
-
 };
-
-
 
 function closeModal(){
   document.getElementById("modal1").style.display = "none";
@@ -23,28 +20,41 @@ function closeModal(){
 
     $.getJSON("https://spreadsheets.google.com/feeds/list/1mFtnlk9MjHz9smP07SG2uPJ8rkv8dt2_eDR5wqLkilg/od6/public/values?alt=json", function(data) {
 
-
+      // var brucewayne = 'batman';
 
       // create a card
       var newCard = ' ';
       for (var i = 0; i < data.feed.entry.length; i++) {
+
+
+        // variables
+        var userName = (data.feed.entry[i]['gsx$name']['$t']); // this works
+        var urlLink = (data.feed.entry[i]['gsx$personalwebsite']['$t']); // this works
+
+
         newCard += '<div class="card small hoverable">';
         newCard += ' <div class="card-image">';
-        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" data-type="website">';
+        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" class="websitelink">';
         newCard += '</a>';
         newCard += '</div>';
         newCard += '<div class="card-content">';
-        newCard += '<p class="card-title" data-type="name"><a href="#" data-type="twitterHandle">Jon Lewis</a></p>';
-        newCard += '<p class="location" data-type="location">HNL / SFO / DC</p>';
-        newCard += '<p class="" data-type="about">Designer + Feminist + Black Man. Founder of @CircaVictor.Formerly lead design @ElevateLabs (2014 App of the Year) @designerbridge Alum #aloha</p>';
+        newCard += '<p class="card-title"><a href="#" class="twitterHandle">' + userName + '</a></p>'; // this works
+        newCard += '<p class="role" ></p>';
+        newCard += '<p class="about">' + data.feed.entry[i]['gsx$twitterhandles']['$t'] + '</p>'; // this works
         newCard += '</div>';
         newCard += '<div class="card-action">';
-        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" class="twitterIcon"><img src="Images/twitter.svg" alt=""></a>';
-        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" class="websitelink right" target="_blank" data-type="website">website</a>';
+        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" class="websitelink twitterIcon"><img src="Images/twitter.svg" alt=""></a>';
+        newCard += '<a href="' + urlLink + ' " class="websitelink right" target="_blank">website</a>'; // this works
         newCard += '</div>';
         newCard += '</div>';
-      }; // end loop
+
+
+
+      };
+
       document.getElementById('rightCol').innerHTML = newCard;
+
+      // newCard.getElementByClassName('about').html = ' batman';
 
 
 
@@ -71,4 +81,3 @@ function closeModal(){
       // // $('.card-action').append('<a href="#">');
 
     }); // End Get Json
-
