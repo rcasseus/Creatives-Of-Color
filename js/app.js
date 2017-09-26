27b3b101edc2dad3aa50113045819071d7f054af
rcasseus <rashan.casseus@gmail.com>
2017-09-26 00:54:49 -0400

    updates to footer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
 // display modal window
-
-function displayModal() { // codekit ignore:line
-  document.getElementById('demo').innerHTML = "Clicked"; // this worked!!!!!!
+function displayModal() {
   document.getElementById('modal1').style.display = "block"; // this worked!!!!!!
   console.log("Donate button clicked");
-};
+}
 
+// close modal window
 function closeModal(){
   document.getElementById("modal1").style.display = "none";
   console.log("Close button clicked");
 
-};
-
-
-
+}
 
 
 // Getting all records and stuff from the database
@@ -30,21 +26,28 @@ function closeModal(){
         // variables
         var userName = (data.feed.entry[i]['gsx$name']['$t']); // this works
         var urlLink = (data.feed.entry[i]['gsx$personalwebsite']['$t']); // this works
+        var about = (data.feed.entry[i]['gsx$about']['$t']); // this works
+        var userImg = (data.feed.entry[i]['gsx$picture']['$t']); // this works
+        var twitterHandle = (data.feed.entry[i]['gsx$twitterhandles']['$t']); // this works
+        var userRole = (data.feed.entry[i]['gsx$role']['$t']);
+        var userLoc = (data.feed.entry[i]['gsx$location']['$t']);
 
 
-        newCard += '<div class="card small hoverable">';
+        // Card template
+        newCard += '<div class="col s12 m4 l4 ">';
+        newCard += '<div class=" card small hoverable">';
         newCard += ' <div class="card-image">';
-        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" class="websitelink">';
-        newCard += '</a>';
+        newCard += '<img src="'+userImg+'" alt="">';
         newCard += '</div>';
         newCard += '<div class="card-content">';
-        newCard += '<p class="card-title"><a href="#" class="twitterHandle">' + userName + '</a></p>'; // this works
-        newCard += '<p class="role" ></p>';
-        newCard += '<p class="about">' + data.feed.entry[i]['gsx$twitterhandles']['$t'] + '</p>'; // this works
+        newCard += '<p class="card-title"><a href="'+ twitterHandle+'" class="twitterHandle">' + userName + '</a></p>'; // this works
+        newCard += '<p class="role">'+ userLoc +'</p>';
+        newCard += '<p class="about">' + about + '</p>'; // this works
         newCard += '</div>';
         newCard += '<div class="card-action">';
-        newCard += '<a href="https://twitter.com/umiworkshop?lang=en" target="_blank" class="websitelink twitterIcon"><img src="Images/twitter.svg" alt=""></a>';
+        newCard += '<a href=" '+twitterHandle+' " target="_blank" class="websitelink twitterIcon"><img src="images/twitter.png" alt="twitter icon"></a>';
         newCard += '<a href="' + urlLink + ' " class="websitelink right" target="_blank">website</a>'; // this works
+        newCard += '</div>';
         newCard += '</div>';
         newCard += '</div>';
 
@@ -52,32 +55,17 @@ function closeModal(){
 
       };
 
-      document.getElementById('rightCol').innerHTML = newCard;
-
-      // newCard.getElementByClassName('about').html = ' batman';
+      document.getElementById('rightColContent').innerHTML = newCard;
 
 
-
-
-
-
-
-
-
-      //   for (var i = 0; i < data.feed.entry.length; i++) {
-      //     var cardImage = "<div class='card-image'>"+"<img src='images/sample-1.jpg'>"+"<span class='card-title'>" ;
-      //     var cardContent =  "<div class='card-content'>";
-      //
-      //     $("").append
-      //
-      //     // $("#rightCol").append('<div class="card">' + cardImage, cardContent);
-      //     // doesnt work $('.card').clone([i]).appendTo('#rightCol');
-      //
-      //     // need to assign data to correct sections
-      //     // $('.card-title').text(data.feed.entry[i]['gsx$name']['$t']);
-      //
-      // } // end of for loop
-      // // $('.card').append('<div class="card-content">' , '<div class="card-action">');
-      // // $('.card-action').append('<a href="#">');
 
     }); // End Get Json
+
+
+// get date for copyright
+
+function currentYear(){
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("copyRightYear").innerHTML = n;
+};
