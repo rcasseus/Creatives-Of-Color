ff02de7b7074e86234b2dd77b3ff4b1f7acbb732
rcasseus <rashan.casseus@gmail.com>
2018-11-03 20:36:30 -0400

    Nov 3, 2018 Backend update
    Redid the connection to the spreadsheet. Now it runs off the sheet created by the form.

--- js/app.js
+++ js/app.js
@@ -14,9 +14,8 @@ function closeModal(){
 
 // Getting all records and stuff from the database
 
-    $.getJSON("https://spreadsheets.google.com/feeds/list/1mFtnlk9MjHz9smP07SG2uPJ8rkv8dt2_eDR5wqLkilg/od6/public/values?alt=json", function(data) {
+    $.getJSON("https://spreadsheets.google.com/feeds/list/1mFtnlk9MjHz9smP07SG2uPJ8rkv8dt2_eDR5wqLkilg/1/public/values?alt=json", function(data) {
 
-      // var brucewayne = 'batman';
 
       // create a card
       var newCard = ' ';
@@ -24,25 +23,27 @@ function closeModal(){
 
 
         // variables
-        var userName = (data.feed.entry[i]['gsx$name']['$t']); // this works
-        var urlLink = (data.feed.entry[i]['gsx$personalwebsite']['$t']); // this works
-        var about = (data.feed.entry[i]['gsx$about']['$t']); // this works
-        var userImg = (data.feed.entry[i]['gsx$picture']['$t']); // this works
-        var twitterHandle = (data.feed.entry[i]['gsx$twitterhandles']['$t']); // this works
-        var userRole = (data.feed.entry[i]['gsx$role']['$t']);
-        var userLoc = (data.feed.entry[i]['gsx$location']['$t']);
+        var userName = (data.feed.entry[i]["gsx$enteryourfullname"]['$t']); // this works
+        var urlLink = (data.feed.entry[i]["gsx$personalwebsiteurl"]['$t']); // this works
+        var about = (data.feed.entry[i]["gsx$tellusaboutyourself"]['$t']); // this works
+        var userImg = (data.feed.entry[i]["gsx$addaprofilepicture"]['$t']); // this works
+        var twitterHandle = (data.feed.entry[i]["gsx$linktotwitteraccount"]['$t']); // this works
+        var userRole = (data.feed.entry[i]["gsx$whatdoyou"]['$t']);
+        var userLoc = (data.feed.entry[i]["gsx$whereareyoulocated"]['$t']);
+
 
 
         // Card template
         newCard += '<div class="col s12 m4 l4 ">';
         newCard += '<div class=" card small hoverable">';
         newCard += ' <div class="card-image">';
-        newCard += '<img src="'+userImg+'" alt="">';
+        newCard += '<img src='+userImg+' alt="">';
         newCard += '</div>';
         newCard += '<div class="card-content">';
-        newCard += '<p class="card-title"><a href="'+ twitterHandle+'" class="twitterHandle">' + userName + '</a></p>'; // this works
+        newCard += '<p class="card-title"><a href="'+ twitterHandle+'" class="twitterHandle"  target=”_blank”>' + userName + '</a></p>'; // this works
         newCard += '<p class="role">'+ userRole +'</p>';
         newCard += '<p class="about">' + about + '</p>'; // this works
+        newCard += '<p class="role">' + userLoc + '</p>'; // this works
         newCard += '</div>';
         newCard += '<div class="card-action">';
         newCard += '<a href=" '+twitterHandle+' " target="_blank" class="websitelink twitterIcon"><img src="images/twitter.png" alt="twitter icon"></a>';
